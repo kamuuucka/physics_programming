31d68b2ad15af72d753768b98775ec1202464a10
Kamila Matuszak <kamila.matuszak@list.pl>
2022-03-31 20:46:30 +0200

    Final assignment v1
    One border works. Borders are square

---
+++ GXPEngine2022BB/GXPEngine2022BB/GXPEngine/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+
+
+internal class Ball : EasyDraw
+{
+    public int radius;
+    float speed;
+
+    public Vec2 position;
+
+    public Ball (int radius, Vec2 position, float speed = 5) : base (radius*2 + 1, radius * 2 + 1)
+    {
+        this.radius = radius;
+        this.position = position;   
+        this.speed = speed;
+
+        SetOrigin(radius, radius);
+
+        Draw(255, 255, 255);
+    }
+
+    private void Draw(byte red, byte green, byte blue)
+    {
+        Fill(red, green, blue);
+        Stroke(red, green, blue);
+        Ellipse(radius, radius, radius * 2, radius * 2);
+    }
+
+    private void FollowMouse()
+    {
+        position.SetXY(Input.mouseX, Input.mouseY);
+    }
+
+    public void UpdateScreenPosition()
+    {
+        x = position.x;
+        y = position.y;
+    }
+
+    public void Step()
+    {
+        FollowMouse();
+        UpdateScreenPosition();
+    }
+}
+
---
+++ GXPEngine2022BB/GXPEngine2022BB/GXPEngine/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+
+internal class Line : GameObject
+{
+    private EasyDraw line;
+
+    public Vec2 start;
+    public Vec2 end;
+    public Line(Vec2 start, Vec2 end)
+    {
+        line = new EasyDraw(game.width, game.height);
+        this.start = start;
+        this.end = end;
+        DrawLine();
+        game.AddChild(line);
+    }
+
+    public void DrawLine()
+    {
+        line.Line(start.x, start.y, end.x, end.y);
+        line.SetColor(34, 234, 186);
+        Console.WriteLine("Line drawn {0}, {1}", start, end);
+    }
+}
+
